6059754d74b9aa8c250dac56f9f63951b9aa024b
SPCRLYST <tlairdsmith@yahoo.com>
2017-09-29 20:17:22 -0400

    Update Bike_Data_Pull.R

--- Bike_Data_Pull.R
+++ Bike_Data_Pull.R
@@ -11,8 +11,11 @@ sql_stations <- "SELECT * FROM `bigquery-public-data.new_york.citibike_stations`
 # Execute the query and store the result for citi stations in NY
 bike_stations <- query_exec(sql_stations, project = project, use_legacy_sql = FALSE)
 
-# sql code to pull 1000 trips
-sql_trips <- "SELECT * FROM `bigquery-public-data.new_york.citibike_trips` LIMIT 1000"
+# sql code to pull all trips
+sql_trips <- "SELECT * 
+              FROM `bigquery-public-data.new_york.citibike_trips`
+              WHERE usertype = 'Subscriber'
+              AND gender = 'female'"
 # Execute the query and store the result for trips in NY
-bike_trips <- query_exec(sql_trips, project = project, use_legacy_sql = FALSE)
+bike_trips <- query_exec(sql_trips, project = project, use_legacy_sql = FALSE, max_pages = Inf)
 
