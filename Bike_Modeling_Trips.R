34170f0a38fc1671082373b64959a548dba3d84f
SPCRLYST <tlairdsmith@yahoo.com>
2017-10-11 21:16:34 -0400

    Update Bike_Modeling_Trips.R

--- Bike_Modeling_Trips.R
+++ Bike_Modeling_Trips.R
@@ -8,3 +8,5 @@ library(timeDate)
 # reading data from previously created trip data; Bike_Modeling.R
 start_trips <- readRDS(gzcon(url("https://github.com/SPCRLYST/BikeProject/blob/master/start_trips_agg.Rds?raw=true")))
 end_trips <- readRDS(gzcon(url("https://github.com/SPCRLYST/BikeProject/blob/master/end_trips_agg.Rds?raw=true")))
+start_bike_stations <- readRDS(gzcon(url("https://github.com/SPCRLYST/BikeProject/blob/master/start_bike_stations.Rds?raw=true")))
+end_bike_stations <- readRDS(gzcon(url("https://github.com/SPCRLYST/BikeProject/blob/master/end_bike_stations.Rds?raw=true")))
